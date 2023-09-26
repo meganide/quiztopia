ec0ec5beaf3990407f549821ee62c01f1174267a
Meganide <renashassan@hotmail.com>
2023-09-26 14:37:07 +0200

    feat: register points

---
+++ src/functions/registerPoints/helpers.ts
@@ -0,0 +1,19 @@
+import { db } from "@/services"
+
+export async function savePoints(
+  quizId: string,
+  username: string,
+  points: number
+) {
+  const params = {
+    TableName: "Quiztopia",
+    Item: {
+      PK: `q#${quizId}`,
+      SK: `u#${username}#p#${points}`,
+      EntityType: "Points",
+      Points: points
+    }
+  }
+
+  await db.put(params).promise()
+}
